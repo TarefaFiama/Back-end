23d2fb067791119772448a20f5d234ae96e1e8df
DevCaioBO <caiobuenooliveira1210@gmail.com>
2025-05-20 14:16:00 -0300

    feat: para todos acessarem :o

---
+++ src/main/java/com/evento/participar/ParticiparApplication.java
@@ -0,0 +1,13 @@
+package com.evento.participar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticiparApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticiparApplication.class, args);
+	}
+
+}
